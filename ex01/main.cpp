ab8611e1ad7c4feb8f7fce35cfe4541e333dd401
Maja Kijewska <majkijew@student.42heilbornn.de>
2026-03-31 18:13:51 +0200

    final commit

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 #include "Serializer.hpp"
 
+#include <string>
+
+
+//just a helper to visualise what we r doing here
+std::string toHex(uintptr_t raw){
+	std::string hex = "0123456789abcdef";
+	std::string res;
+
+	while (raw > 0) {
+		res = hex[raw % 16] + res;
+		raw /= 16;
+	}
+	return(res);
+}
+
 int main() {
 	
 	Data ptr;
@@ -9,14 +24,18 @@ int main() {
 	uintptr_t raw;
 
 	ptr.age = 13;
-	ptr.name = "slawyslaw";
-
-
+	ptr.name = "blab";
 	raw = Serializer::serialize(&ptr);
-
-	//to test
-	// comparePtr = nullptr;
+	// std::cout << "ptr ad "<< &ptr << std::endl;
+	// std::cout << "raw val 0x"<< toHex(raw) << std::endl;
+	
 	comparePtr = Serializer::deserialize(raw);
 	std::string result = (&ptr == comparePtr) ? "true" : "false";
 	std::cout << result << std::endl;
+
+	// std::cout << "ptr values "<< ptr.age << " "<< ptr.name << std::endl;
+	// std::cout << "comparePtr values "<< comparePtr->age << " "<< comparePtr->name << std::endl;
+
+	// std::cout << "ptr adres "<< &ptr << std::endl;
+	// std::cout << "comparePtr adres "<< comparePtr << std::endl;
 } 
